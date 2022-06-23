8c0fd845ecdae6607c0638b13901aafbe7994383
Erin Ranta <erinran99@gmail.com>
2022-06-22 22:56:45 -0400

    added table, styled sales page, made form work, changed in js for table

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 'use strict';
 
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-let storeFormEl = document.getElementById('store-form');
+let totalSalesArray = [];
+
+
+function generateNumberBetween(min, max) {
+  return Math.round(Math.random() * (max - min)) + min;
+}
 
 function Store(name, minCust, maxCust, avgCookiePerCust) {
   this.name = name;
@@ -12,85 +17,124 @@ function Store(name, minCust, maxCust, avgCookiePerCust) {
   this.avgCookiePerCust = avgCookiePerCust;
 }
 
-    Store.prototype.generateCookiesPerHour = function() {
+Store.prototype.generateCookiesPerHour = function() {
   for (let i = 0; i < hours.length; i++) {
     let customersAtHour = generateNumberBetween(this.minCust, this.maxCust);
-      let salesAtHour = Math.round(customersAtHour * this.avgCookiePerCust);
+    let salesAtHour = Math.round(customersAtHour * this.avgCookiePerCust);
     this.hourlySales.push(salesAtHour);
     this.totalSales += salesAtHour;
+    totalSalesArray.push(this.totalSales);
   }
 };
 
+///render hours header
+
+function renderHeader() {
+  let tableEl = document.getElementById('sales-data');
+  let tableRowEl = document.createElement('tr');
+  let startEl = document.createElement('td');
+  let endEl = document.createElement('td');
+  startEl.classList.add('empty');
+  endEl.classList.add('total');
+  
+  tableEl.appendChild(tableRowEl);
+  tableRowEl.appendChild(startEl);
+  startEl.textContent = '';
+  
+  for (let i=0; i<hours.length; i++) {
+    let newDataCell = document.createElement('td');
+    newDataCell.classList.add('heading');
+    tableRowEl.appendChild(newDataCell);
+    newDataCell.textContent = hours[i];
+  }
+  
+  tableEl.appendChild(tableRowEl);
+  tableRowEl.appendChild(endEl);
+  endEl.textContent = 'Total:';
+}
+
+renderHeader();
+
+Store.prototype.renderData = function() {
+  let tableEl = document.getElementById('sales-data');
+  let rowEl = document.createElement('tr');
+  let nameCell = document.createElement('td');
+  nameCell.textContent = this.name;
+  rowEl.appendChild(nameCell);
+
+  for (let i = 0; i < this.hourlySales.length; i++) {
+    let cellEl = document.createElement('td');
+    cellEl.textContent = this.hourlySales[i];
+    rowEl.appendChild(cellEl);
+  }
+
+  let totalCell = document.createElement('td');
+  totalCell.textContent = this.totalSales;
+  rowEl.appendChild(totalCell);
+  tableEl.appendChild(rowEl);
+};
+
+let seattle = new Store('Seattle', 10, 20, 3);
+let lima = new Store('Lima', 1, 50, 2.6);
+let paris = new Store('Paris', 5, 25, 4);
+let london = new Store('London', 13, 19, 6);
+let newYork = new Store('New York', 2, 15, 3.5);
+
+seattle.generateCookiesPerHour();
+lima.generateCookiesPerHour();
+paris.generateCookiesPerHour();
+london.generateCookiesPerHour();
+newYork.generateCookiesPerHour();
+
+seattle.renderData();
+lima.renderData();
+paris.renderData();
+london.renderData();
+newYork.renderData();
+
+function renderFooter() {
+  let tableEl = document.getElementById('sales-data');
+  let tableRowEl = document.createElement('tr');
+  let startEl = document.createElement('td');
+  let endEl = document.createElement('td');
+  startEl.classList.add('total');
+  endEl.classList.add('total');
+  
+  tableEl.appendChild(tableRowEl);
+  tableRowEl.appendChild(startEl);
+  startEl.textContent = 'Total:';
+  
+  let totalTotal = 0;
+  
+  for ( let i=0; i<hours.length; i++) {
+    let newDataCell = document.createElement('td');
+    newDataCell.classList.add('total');
+    tableRowEl.appendChild(newDataCell);
+    newDataCell.textContent = totalSalesArray[i];
+    totalTotal += totalSalesArray[i];
+  }
+  
+  tableEl.appendChild(tableRowEl);
+  tableRowEl.appendChild(endEl);
+  endEl.textContent = totalTotal;
+}
 
 
+renderFooter();
+
+function clearTable() {
+  let tableElement = document.getElementById('sales-data');
+  tableElement.innerHTML = '';
+};
+
+let storeFormEl = document.getElementById('store-form');
 storeFormEl.addEventListener('submit', function(event) {
   event.preventDefault();
-  let name = event.target.location.value;
-  let minCust = parseInt(event.target.min.value);
-  let maxCust = parseInt(event.target.max.value);
-  let avgCookies = parseInt(event.target.avgCookies.value);
-
+  let name = event.target.storeName.value;
+  let minCust = parseInt(event.target.minCust.value);
+  let maxCust = parseInt(event.target.maxCust.value);
+  let avgCookies = parseInt(event.target.avgSale.value);
   let store = new Store(name, minCust, maxCust, avgCookies);
   store.generateCookiesPerHour();
   store.renderData();
 });
-
-
-function generateNumberBetween(min, max) {
-  return Math.round(Math.random() * (max - min)) + min;
-}
-
-
-Store.prototype.renderData = function() {
-  let tableEl = document.getElementById('sales-data');
-  let rowEl = document.createElement('tr')
-  
- let nameCell = document.createElement('td');
-  nameCell.textContent = this.name;
-  rowEl.appendChild(nameCell);
-
-
-  for (let i = 0; i < this.hourlySales.length; i++) {
-  let cellEl = document.createElement('td');
-      cellEl.textContent = this.hourlySales[i];
-    rowEl.appendChild(cellEl);
-  }
-
-  
-  let totalCell = document.createElement('td');
-      totalCell.textContent = this.totalSales;
-  rowEl.appendChild(totalCell);
-
-  tableEl.appendChild(rowEl);
-};
-
-function generateCookiesPerHour(minCust, maxCust, avgCookiesPerCust) {
-    let hourlySales = [];
-  
-    for (let i = 0; i < hours.length; i++) {
-      let customersAtHour = generateNumberBetween(minCust, maxCust);
-      let salesAtHour = customersAtHour * avgCookiesPerCust;
-      hourlySales.push(salesAtHour);
-    }
-    return hourlySales;
-  }
-
-let seattle = new Store('Seattle', 23, 65, 6.3);
-let tokyo = new Store('tokyo', 3, 24, 1.2);
-let dubai = new Store('Dubai', 11, 38, 3.7);
-let paris = new Store('Paris', 20, 38, 2.3);
-let lima = new Store('Lima', 2, 16, 4.6);
-
-
-
-seattle.generateCookiesPerHour();
-tokyo.generateCookiesPerHour();
-dubai.generateCookiesPerHour();
-paris.generateCookiesPerHour();
-lima.generateCookiesPerHour();
-
-seattle.renderData();
-tokyo.renderData();
-dubai.renderData();
-paris.renderData();
-lima.renderData();
